87547d7763e73f4d07d5484c534e21cced6ea9cc
AntonStoilovskyi <stoilovskyi.anton@gmail.com>
2022-08-11 22:26:34 +0200

    init jest

---
+++ tests/errorBug.spec.ts
@@ -0,0 +1,18 @@
+// import { ErrorBag } from '../index';
+// import { ErrorBag } from ;
+const ErrorBag = require('../src/ErrorBag')
+describe('ErrorBag', () => { 
+    // let errorBag: ErrorBag;
+
+    // beforeEach(() => {
+    // });
+    
+    
+    it('hasError() returns true if has any error', () => {
+        // const errorBag = new ErrorBag();
+        // errorBag.add(new Error('some test error'));
+
+        // expect(errorBag.hasErrors()).toBe(true);
+        
+    });    
+}) 
