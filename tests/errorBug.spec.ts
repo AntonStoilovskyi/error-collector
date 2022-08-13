3c1194b2ea8cea6ca293cd82b6898ff18437778d
AntonStoilovskyi <stoilovskyi.anton@gmail.com>
2022-08-13 18:19:47 +0200

    getErrors() and hasError() tests

--- tests/errorBug.spec.ts
+++ tests/errorBug.spec.ts
@@ -1,18 +1,41 @@
 import { ErrorBag } from '../index';
 
 describe('ErrorBag', () => { 
-    describe('method hasError() returns', () => {
     let errorBag: ErrorBag;
-
+    
     beforeEach(() => {
         errorBag = new ErrorBag();
+    });
+    describe('method hasError() returns', () => {
+        describe('true', () => { 
+            it('when has any error', () => {
+                errorBag.add(new Error('some test error'));
+                expect(errorBag.hasErrors()).toBe(true);        
+            });
+         });
+         describe('false', () => { 
+            it('when bag is empty', () => {
+                expect(errorBag.hasErrors()).toBe(false);
+            });     
+            it('when method add() tried to add non Error type argument', () => {
+                errorBag.add('test');
+                expect(errorBag.hasErrors()).toBe(false);
+            });
+        });
+    });
+    describe('method getErrors() returns', () => {
+        it('empty Error array when bag is empty', () => {
+            expect(errorBag.getErrors()).toBeInstanceOf(Array);
+            expect(errorBag.getErrors().length).toBe(0);
+        });
+        it('non empty Error array when bag is not empty', () => {
+            const errors = ['first error', 'second error'];
+            errors.forEach(error => {
+                errorBag.add(new Error(error));
+            });
+            
+            expect(errorBag.getErrors()).toBeInstanceOf(Array);
+            expect(errorBag.getErrors().length).toBe(errors.length);            
+        });
     })
-    it('true if has any error', () => {
-        errorBag.add(new Error('some test error'));
-        expect(errorBag.hasErrors()).toBe(true);        
-     });
-     it('false if bag is empty', () => {
-        expect(errorBag.hasErrors()).toBe(false);
-     })
- })
 }) 
